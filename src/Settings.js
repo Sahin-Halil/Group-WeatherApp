e4c987af087edfaf78ca68cba5c9c60911d4f0f1
mollyhall205 <mollyhall205@gmail.com>
2025-03-16 19:14:57 +0000

    Initial homepage setup

---
+++ src/Settings.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Settings = () => {
+  return (
+    <div>
+      <h1>Settings Page</h1>
+      <p>This is where user settings will go.</p>
+    </div>
+  );
+};
+
+export default Settings;
