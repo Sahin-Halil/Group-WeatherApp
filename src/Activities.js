f8394b3277e513842d9aa2c31cdc0eb9a2ae68dd
“Sahin” <sahinhalil1311@gmail.com>
2025-03-18 11:02:31 +0000

    Added initial layout for Activities page

--- src/Activities.js
+++ src/Activities.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Navbar from "./Navbar";
+import "./Activities.css";
 
 const Activities = () => {
   return (
-    <div>
+    <div className="activities-container">
       <h1>Activities Page</h1>
       <p>This is where activity recommendations will go.</p>
+      <Navbar />
     </div>
   );
 };
