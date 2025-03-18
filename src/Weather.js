61d894c1ac53c621361fd67b1d2d88ea27fb6bba
“Sahin” <sahinhalil1311@gmail.com>
2025-03-18 10:46:14 +0000

    Moved bottom navbar to a separate component for reuse

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import "./Weather.css";
+import Navbar from "./Navbar";
 
 import ClearDay from "./weather-icons/sun.png";
 import CloudyDay from "./weather-icons/cloudy-day.png";
@@ -102,21 +102,7 @@ const Weather = () => {
         )}
         </div>
       )}
-
-    <div className="bottom-nav">
-      <Link to="/Activities" className="nav-button">
-        <img src="/activities-icon.png"/>
-        Activities
-      </Link>
-      <Link to="/Weather" className="nav-button">
-        <img src="/weather-icon.png"/>
-        Weather
-      </Link>
-      <Link to="/Settings" className="nav-button">
-        <img src="/settings-icon.png"/>
-        Settings
-      </Link>
-    </div>
+    <Navbar />
     </div>
     );
   };
